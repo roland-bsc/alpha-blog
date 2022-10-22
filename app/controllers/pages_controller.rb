df31ae2bb8b48ac5e7e1ac4b500f46e06ebca4aa
roland-bsc <rabellaatbsc@gmail.com>
2022-10-21 23:17:09 -0700

    iintial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home #you need an erb file for this called home.html.erb
+  end
+
+  def about #you need an erb file for this called about.html.erb
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home' #this goes to a controller
+  get 'about', to: 'pages#about' #another controller action called 'about'
+end
