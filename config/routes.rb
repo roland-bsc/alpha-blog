9497d4113d39dad7e0bf9fe862d1c33b0eb52b39
roland-bsc <rabellaatbsc@gmail.com>
2022-10-22 21:47:43 -0700

    new commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home' #this goes to a controller
   get 'about', to: 'pages#about' #another controller action called 'about'
+
+  resources :articles, only: [:show]
+  
 end
