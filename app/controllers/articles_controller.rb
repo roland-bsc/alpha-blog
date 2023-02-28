b80f8d2fdd8e5dc7b42023231aaa485385dec4a3
roland-bsc <rabellaatbsc@gmail.com>
2023-02-27 17:38:21 -0800

    added edit.html.erb & edit and upate routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,28 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    # we need to get the instance object we want to edit by getting the value in the params hash
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article was created successfully." #flash helper hash for displaying messages
       redirect_to @article # this line is a short cut to 'redirect_to article_path(@article)'
     else
-      render 'new' #if the article instance variable value didn't get save, plase show new form again
+      render 'new' #if the article instance variable value didn't get save, plase show 'new' form again
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was created successfully." 
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
