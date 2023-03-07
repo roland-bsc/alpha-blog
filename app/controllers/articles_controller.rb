404fcbb8129d747b124c2068d328f0aff1875cb8
roland-bsc <rabellaatbsc@gmail.com>
2023-03-06 17:59:19 -0800

    added bootstrap, css, popper.js

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +14,10 @@ class ArticlesController < ApplicationController
 
   def edit
     # we need to get the instance object we want to edit by getting the value in the params hash
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully." #flash helper hash for displaying messages
       redirect_to @article # this line is a short cut to 'redirect_to article_path(@article)'
@@ -27,13 +27,27 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was created successfully." 
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private 
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
